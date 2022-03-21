203e7c85b949bbd633941544f991f0e468acfe63
Withered Flowers <withered-flowers@users.noreply.github.com>
2022-03-21 21:54:00 +0700

    feat: add default initial textView value

--- app/src/main/java/dev/witheredflowers/diceroller/MainActivity.kt
+++ app/src/main/java/dev/witheredflowers/diceroller/MainActivity.kt
@@ -2,10 +2,24 @@ package dev.witheredflowers.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//      Find the button
+        val rollButton: Button = findViewById(R.id.button);
+
+//      Set the onClickListener for the Button
+        rollButton.setOnClickListener {
+//      Create the toast and show it
+//          Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT).show()
+            val resultTextView: TextView = findViewById(R.id.textView)
+            resultTextView.text = "6"
+        }
     }
 }
