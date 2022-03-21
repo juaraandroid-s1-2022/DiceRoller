b9bdb146ad83f001da583ff71c43cd4e81762997
Withered Flowers <withered-flowers@users.noreply.github.com>
2022-03-21 22:04:15 +0700

    feat: commenting the code

--- app/src/main/java/dev/witheredflowers/diceroller/MainActivity.kt
+++ app/src/main/java/dev/witheredflowers/diceroller/MainActivity.kt
@@ -7,34 +7,33 @@ import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_main)
 
 //      Find the button
-        val rollButton: Button = findViewById(R.id.button);
+		val rollButton: Button = findViewById(R.id.button);
 
 //      Set the onClickListener for the Button
-        rollButton.setOnClickListener {
-//      Create the toast and show it
-//            Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT).show()
-//            val resultTextView: TextView = findViewById(R.id.textView)
-//            resultTextView.text = "6"
+		rollButton.setOnClickListener { rollDice() }
+	}
 
-            rollDice()
-        }
-    }
+    /**
+     * Roll the dice and update the screen with the result.
+     */
+	private fun rollDice() {
+//      Create new Dice object with 6 sides and roll it
+		val dice = Dice(6);
+		val diceRoll = dice.roll()
 
-    private fun rollDice() {
-        val dice = Dice(6);
-        val diceRoll = dice.roll()
-        val resultTextView: TextView = findViewById(R.id.textView)
-        resultTextView.text = diceRoll.toString()
-    }
+//      Update the screen with the dice roll
+		val resultTextView: TextView = findViewById(R.id.textView)
+		resultTextView.text = diceRoll.toString()
+	}
 }
 
 class Dice(val numSides: Int) {
-    fun roll(): Int {
-        return (1..numSides).random()
-    }
+	fun roll(): Int {
+		return (1..numSides).random()
+	}
 }
