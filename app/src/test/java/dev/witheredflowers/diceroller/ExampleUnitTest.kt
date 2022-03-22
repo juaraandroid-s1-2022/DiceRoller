2728b8f2e25419c10bd58c10b53a32f9528378a6
Withered Flowers <withered-flowers@users.noreply.github.com>
2022-03-22 20:25:04 +0700

    feat: Modify unit test

--- app/src/test/java/dev/witheredflowers/diceroller/ExampleUnitTest.kt
+++ app/src/test/java/dev/witheredflowers/diceroller/ExampleUnitTest.kt
@@ -10,8 +10,14 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+    // @Test
+    // fun addition_isCorrect() {
+    //     assertEquals(5, 2 + 3)
+    // }
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun generates_number() {
+        val dice = Dice(6)
+        val rollResult = dice.roll()
+        assertTrue("The value of rollResult was not between 1 and 6", rollResult in 1..6)
     }
 }
